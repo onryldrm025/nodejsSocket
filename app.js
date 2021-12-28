53cceff72078603d4ed037016fa8d14a8de14275
Onur yildirim <onryldrm025@hotmail.com>
2021-12-28 16:54:05 +0300

    add prosedure

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const { Server } = require('socket.io');
+const config = require('./dbconfig')
 const app = express();
-var mysql      = require('mysql');
-
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'deneme',
-    port: 3306,
-    insecureAuth : true
-  });
+const sql = require('mssql')
 
 
-connection.connect();
+function useradd(req){
+    sql.connect(config).then(pool=>{
+        let request = new sql.Request(pool)
+        request.input('nva_sirketkodu',sql.NVarChar(30),req.sirketkodu)
+        request.input('nva_sicilkodu',sql.NVarChar(30),req.sicilkodu)
+        request.input('nva_sicilismi',sql.NVarChar(sql.MAX),req.sicilismi)
+        request.execute('sp_noe_m_w_mrp_hareket_giriscikis_ekle').then((result)=>{
+            return result
+        }).catch(e=>{
+            console.log(e)
+        })
+    })
+}
+// var mysql      = require('mysql');
+
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'deneme',
+//     port: 3306,
+//     insecureAuth : true
+//   });
+
+
+// connection.connect();
  
-connection.query('SELECT * from kullanıcılar', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0]);
-});
+// connection.query('SELECT * from kullanıcılar', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0]);
+// });
 
 const sunucu = app.listen(9000, () => {
     console.log('server çalışıyor');
 })
 
-app.get('/hi', (req, res) => {
-    res.json('hi');
-})
 
 const io = new Server(sunucu);
 
@@ -36,8 +50,8 @@ io.on('connection', (socket) => {
     socket.on('userAdd', (data) => {
         io.emit('message', data)
         var datass = JSON.parse(data)
-        connection.query(`insert into kullanıcılar(name) values('${datass.isim}')`)
-        console.log(data);
+        useradd(datass)
+        console.log(datass);
     })
     socket.on('msg', (data) => {
         console.log(data)
