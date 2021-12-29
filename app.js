199f1d22e057d53bc0b6c62b978401bdaf46922f
Onur yildirim <onryldrm025@hotmail.com>
2021-12-29 16:44:52 +0300

    add prosedure

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const { Server } = require('socket.io');
 const config = require('./dbconfig')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 const app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 const sql = require('mssql')
 
 
 function useradd(req){
-    sql.connect(config).then(pool=>{
+     sql.connect(config).then(pool=>{
         let request = new sql.Request(pool)
         request.input('nva_sirketkodu',sql.NVarChar(30),req.sirketkodu)
         request.input('nva_sicilkodu',sql.NVarChar(30),req.sicilkodu)
         request.input('nva_sicilismi',sql.NVarChar(sql.MAX),req.sicilismi)
+        request.input('tarih',sql.DateTime,(req.tarih || req.zaman ))
+        request.input('onofflie',sql.Bit,(req.onoff || 0))
+        request.input('tablet',sql.NVarChar(50),req.tablet)
         request.execute('sp_noe_m_w_mrp_hareket_giriscikis_ekle').then((result)=>{
             return result
         }).catch(e=>{
-            console.log(e)
+            console.log('Eroro:' + e)
         })
+    })
+}
+
+function allAdd(req,tblt){
+    return new Promise((resolve,reject)=>{
+        sql.connect(config).then(pool=>{
+            let request = new sql.Request(pool)
+            request.input('nva_sirketkodu',sql.NVarChar(30),req.sirketkodu)
+            request.input('nva_sicilkodu',sql.NVarChar(30),req.sicilkodu)
+            request.input('nva_sicilismi',sql.NVarChar(sql.MAX),req.sicilismi)
+            request.input('tarih',sql.NVarChar(30),req.zaman.slice(0,-3))
+            request.input('onofflie',sql.Bit,(req.onoff || 1))
+            request.input('tablet',sql.NVarChar(50),tblt)
+            request.execute('sp_noe_m_w_mrp_hareket_giriscikis_ekle').then((result)=>{
+                resolve({data:req ,status:true})
+            }).catch(e=>{
+                reject( {data:req ,status:false})
+            })
+        })
+
     })
 }
 // var mysql      = require('mysql');
@@ -37,6 +67,17 @@ function useradd(req){
 //   console.log('The solution is: ', results[0]);
 // });
 
+app.post('/saveusers',(req,res)=>{
+     var ss = req.body.arr.map((item)=> allAdd(item,
+        req.body.tablet))
+     Promise.all(ss).then((data)=>{
+        res.json({status:true,data})
+     }).catch((e)=>{
+        res.json({status:false,e})
+    })
+   
+})
+
 const sunucu = app.listen(9000, () => {
     console.log('server çalışıyor');
 })
@@ -48,10 +89,10 @@ const io = new Server(sunucu);
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('userAdd', (data) => {
-        io.emit('message', data)
-        var datass = JSON.parse(data)
-        useradd(datass)
-        console.log(datass);
+        io.emit('message', JSON.stringify(data))
+        // var datass = JSON.parse(data)
+        useradd(data)
+        console.log(data);
     })
     socket.on('msg', (data) => {
         console.log(data)
